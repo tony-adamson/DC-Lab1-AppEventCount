d90d049a4d7f7d5bce0ac929d7880c1f126e0737
Anton <antony.adamson@gmail.com>
2023-04-21 14:18:54 +0600

    Add Event Counters to AppDelegate

--- DC-Lab1-AppEventCount/AppDelegate.swift
+++ DC-Lab1-AppEventCount/AppDelegate.swift
@@ -10,10 +10,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var launchingCount = 0
+    var configurationForConnectingCount = 0
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        launchingCount += 1
         return true
     }
 
@@ -22,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        configurationForConnectingCount += 1
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
