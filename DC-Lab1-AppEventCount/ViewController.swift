02da93b6e2dc8ca56bf2197a754530ccf175a56f
Anton <antony.adamson@gmail.com>
2023-04-21 14:34:40 +0600

    Set Up View and ViewController

--- DC-Lab1-AppEventCount/ViewController.swift
+++ DC-Lab1-AppEventCount/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var didFinishLaunchingLabel: UILabel!
+    @IBOutlet weak var configurationForConnectingLabel: UILabel!
+    @IBOutlet weak var willConnectToLabel: UILabel!
+    @IBOutlet weak var sceneDidBecomeActiveLabel: UILabel!
+    @IBOutlet weak var sceneWillResignActiveLabel: UILabel!
+    @IBOutlet weak var sceneWillEnterForegroundLabel: UILabel!
+    @IBOutlet weak var sceneDidEnterBackgroundLabel: UILabel!
+    
+    var finishLaunchingCount = 0
+    var configurationForConnectingCount = 0
+    var willConnectToCount = 0
+    var sceneDidBecomeActiveCount = 0
+    var sceneWillResignActiveCount = 0
+    var sceneWillEnterForegroundCount = 0
+    var sceneDidEnterBackgroundCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
